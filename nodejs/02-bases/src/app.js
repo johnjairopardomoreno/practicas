6d636bbc669583041ca04e0459e31fb7410d1194
John Jairo Pardo Moreno <johnjairopardomoreno@gmail.com>
2025-09-23 21:40:24 -0500

    Practica Sep 23 2025 - Video 24

--- nodejs/02-bases/src/app.js
+++ nodejs/02-bases/src/app.js
@@ -1,1 +1,7 @@
-require('./js-foundation/01-template');
+// const template_exporte = require('./js-foundation/01-template');
+// console.log(template_exporte.email_template);    
+
+
+// Desestructuracion : sintaxis de importacion de modulos
+const {email_template } = require('./js-foundation/01-template');
+console.log(email_template);    
--- nodejs/02-bases/src/js-foundation/01-template.js
+++ nodejs/02-bases/src/js-foundation/01-template.js
@@ -2,8 +2,8 @@ const email_template = `
 <div>
     <h1>Hola {{name}}</h1>
     <p>Gracias por su orden</p>
+    <p>Esperamos que vuelva pronto</p>
 </div>
 `;  
 
-
-console.log(email_template);
+module.exports = { email_template };
